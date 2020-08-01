fa1a89dcc14d7120ed0de87d22a4c8ecfe9be019
Frits Sasia <fritssasia@gmail.com>
2020-08-01 17:27:31 +0800

    change heading & add button

--- js/script.js
+++
@@ -1,8 +0,0 @@
-// Navbar
-$(window).on("scroll", function () {
-    if ($(window).scrollTop() > 50) {
-        $(".navbar").addClass("navbar-active");
-    } else {
-        $(".navbar").removeClass("navbar-active");
-    }
-});
